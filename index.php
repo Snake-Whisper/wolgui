541e5986de7a77e77707099e804bbad05365edd0
snake-whisper <snake-whisper@web-utils.eu>
2019-11-13 23:31:49 +0100

    impl printMachine + fix css

--- index.php
+++ index.php
@@ -10,28 +10,24 @@
   </style>
 </head>
 <body>
-  <div class="machineBox">
-    <div class="buttonBox showAlivePing">
-      <img src="img/redButton.svg" class="buttonVector"></img>
-    </div>
-    <table class="machineStats">
-      <tr>
-        <th>Hostname</th>
-        <td>servername</td>
-      </tr>
-      <tr>
-        <th>IP-Addr</th>
-        <td>192.168.178.39</td>
-      </tr>
-      <tr>
-        <th>MAC-Addr</th>
-        <td>BC:BC:BC:BC:BC</td>
-      </tr>
-      <tr>
-        <th>Protected</th>
-        <td>Yes</td>
-      </tr>
-    </table>
-  </div>
+<?php
+    function printMachine($stats, $commands = array()) {
+      echo "<div class='machineBox'><div class='buttonBox showAlivePing'><img src='img/redButton.svg' class='buttonVector'></img></div><table class='machineStats'>";
+      foreach ($stats as $key => $value) {
+        echo "<tr><th>$key</th><td>$value</td></tr>";
+      }
+      echo "</table>";
+    }
+    $teststats = [
+    "mac" => "BB:BB:BB:BB:BB",
+    "ip"  => "192.168.178.20",
+    "Name" => "Main Server",
+    "ipv6" => "2001:BEEF:BEEF:BEEF:BEEF:BEEF:BEEF",
+    "location" => "Main Room",
+    "comment" => "Mien Server",
+    "owner" => "snmake-whisper",
+    "email" => "snake-whiusper@web-utils.eu"];
+    printMachine($teststats);
+  ?>
 </body>
 </html>
