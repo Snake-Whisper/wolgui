efded9bfa2adbf8070c4c7b45fab0d6e53e41a38
snake-whisper <snake-whisper@web-utils.eu>
2019-12-31 17:29:59 +0100

    implement wolv4 !!!

--- lib/misc.php
+++ lib/misc.php
@@ -1,6 +1,23 @@
 <?php
 
-  function wol($mac, $broadcast) {
+  function v4cidr2mask ($cidr) {
+    $long = 0xffffffff << (32 - $cidr);
+    return long2ip($long);
+  }
+
+  function extractBroadcastIPv4($ipWithMask) {
+    list($ip, $cidr) = explode("/", $ipWithMask);
+    $mask = v4cidr2mask($cidr);
+
+    $wcmask=long2ip( ~ip2long($mask) );
+    $subnet=long2ip( ip2long($ip) & ip2long($mask) );
+    $bcast=long2ip( ip2long($ip) | ip2long($wcmask) );
+
+    return $bcast;
+  }
+
+  function wolv4($mac, $IPv4) {
+    $broadcast = extractBroadcastIPv4($IPv4);
     $hwaddr = pack('H*', preg_replace('/[^0-9a-fA-F]/', '', $mac));
 
     // Create Magic Packet
@@ -22,6 +39,7 @@
     }
 }
 
+
 function execInBackground($cmd) {
     if (substr(php_uname(), 0, 7) == "Windows"){
         pclose(popen("start /B ". $cmd, "r"));
@@ -30,4 +48,16 @@ function execInBackground($cmd) {
         exec($cmd . " > /dev/null &");
     }
 }
- ?>
+
+
+/*function v6cidr2mask ($cidr) {
+  $long = 0xffffffffffffffffffffffffffffffff << (128 - $cidr);
+  return inet_pton($long);
+}*/
+
+//var_dump(v6cidr2mask(127));
+
+
+
+//  wolv4("d4:be:d9:93:60:4d", "192.168.178.39/24");
+?>
