897ac06fa1db60fd7eaae399801afa0a05f8a5bb
snake-whisper <snake-whisper@web-utils.eu>
2019-12-31 12:23:48 +0100

    beautify

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 <head>
   <link rel="stylesheet" href="css/machineBox.css">
   <link rel="stylesheet" href="css/layout.css">
-  <style>
-
-
-
-  </style>
 </head>
 <body>
   <div id='machinesContainer'>
@@ -22,19 +17,6 @@
   }
 
   $bk->getMachines("_h_buildMachinesPanel");
-  /*$teststats = [
-    "Mac" => "BB:BB:BB:BB:BB",
-    "IP"  => "192.168.178.20",
-    "Name" => "Main Server",
-    "IPv6" => "2001:BEEF:BEEF:BEEF:BEEF:BEEF:BEEF:BEEF",
-    "Location" => "Main Room",
-    "Comment" => "Main Server",
-    "Owner" => "snake-whisper",
-    "eMail" => "snake-whisper@web-utils.eu"];
-    for ($i=0; $i < 13; $i++) {
-
-    }*/
-
   ?>
 </div>
 </body>
